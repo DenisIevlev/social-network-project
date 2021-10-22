46186a891b6a1106b3d3fb9f6f559a033d81cded
Denis Ievlev <iievliievdenis@gmail.com>
2021-10-22 22:31:15 +0300

    I added user list pagination

--- src/Components/users/userscontainer.js
+++ src/Components/users/userscontainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Users from './users';
-import {followActionCreator, unfollowActionCreator, setUsersActionCreator} from '../../redux/users-reducer';
+import {followActionCreator, unfollowActionCreator, setUsersActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator} from '../../redux/users-reducer';
 import {connect} from 'react-redux';
 
 let mapStateToProps = (state) => {
   return {
-      users: state.usersPage.users
+      users: state.usersPage.users,
+      pageSize: state.usersPage.pageSize,
+      totalUsersCount: state.usersPage.totalUsersCount,
+      currentPage: state.usersPage.currentPage
   }
 }
 
@@ -13,7 +16,10 @@ let mapDispatchToProps = (dispatch) => {
   return {
         followActionCreator: (userId) => dispatch(followActionCreator(userId)),
         unfollowActionCreator: (userId) => dispatch(unfollowActionCreator(userId)),
-        setUsersActionCreator: (users) => dispatch(setUsersActionCreator(users))
+        setUsersActionCreator: (users) => dispatch(setUsersActionCreator(users)),
+        setCurrentPageActionCreator: (currentPage) => dispatch(setCurrentPageActionCreator(currentPage)),
+        setTotalUsersCountActionCreator: (totalCount) => dispatch(setTotalUsersCountActionCreator(totalCount))
+        
   }
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,11 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_COUNT = 'SET_TOTAL_COUNT';
 
 
 let initialState = {
-  users: [
-  ]
+  users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -33,8 +37,18 @@ const usersReducer = (state = initialState, action) => {
       case SET_USERS:
         return {
             ...state, 
-            users: [...state.users, ...action.users]
+            users: [...action.users]
         }
+        case SET_CURRENT_PAGE:
+          return {
+            ...state,
+            currentPage: action.currentPage
+          }
+          case SET_TOTAL_COUNT:
+            return {
+              ...state,
+              totalUsersCount: action.totalUsersCount
+            }
     default:
       return state;
   }
@@ -60,5 +74,19 @@ export const setUsersActionCreator = (users) => {
   return {
     type: SET_USERS,
     users
+  }
+}
+
+export const setCurrentPageActionCreator = (currentPage) => {
+  return {
+    type: SET_CURRENT_PAGE,
+    currentPage
+  }
+}
+
+export const setTotalUsersCountActionCreator = (totalUsersCount) => {
+  return {
+    type: SET_TOTAL_COUNT,
+    totalUsersCount
   }
 }
